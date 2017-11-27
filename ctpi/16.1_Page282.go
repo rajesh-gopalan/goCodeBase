4540893b35525eb26ed737a565468492e56ecfb6
rajesh-gopalan <rajeshgopalan@gmail.com>
2017-11-26 16:32:42 -0800

    Initial create

---
+++ ctpi/16.1_Page282.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+func main() {
+  var sliceLen int
+  sliceVal := make([]int, sliceLen)
+  var newElement int
+
+  sliceLen = getSliceLen()
+  sliceVal = getSliceVals(sliceLen)
+  newElement = getNewElem()
+
+  fmt.Println("Existing slice value\t: ", sliceVal)
+  var successFlag bool = insertNewElem(sliceVal, newElement)
+  if successFlag {
+    fmt.Println("New slice value\t\t: ", sliceVal)
+  } else {
+    fmt.Println("Erorr: Please satisfy requirements")
+  }
+}
+
+// Supporting functions
+
+func getSliceLen() int {
+  var i int
+  fmt.Println("Enter the length of the slice: ")
+  fmt.Scan(&i)
+  return i
+}
+
+func getSliceVals(sVal int) []int {
+  sliceVal := make([]int, sVal)
+  fmt.Println("Enter the slice values separated by space: ")
+  for i := 0; i < sVal; i++ {
+      fmt.Scan(&sliceVal[i])
+    }
+  return sliceVal
+}
+
+func getNewElem() int {
+  var nElem int
+  fmt.Println("Enter the newElement to be inserted: ")
+  fmt.Scan(&nElem)
+  return nElem
+}
+
+func insertNewElem(sVal []int, newElem int) bool {
+  var loopVar int = len(sVal) - 1
+  if sVal[loopVar] != 0 || newElem <= 0  {
+    return false
+  }
+  for loopVar >= 0 && sVal[loopVar - 1] > newElem {
+    sVal[loopVar]     = sVal[loopVar - 1]
+    sVal[loopVar - 1] = newElem
+    loopVar--
+  }
+  return true
+}
